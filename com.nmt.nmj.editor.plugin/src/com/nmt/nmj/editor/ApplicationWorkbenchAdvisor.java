0f32b321f91d455c48fa20372e2aed7207c74123
Lucas Amador <lucazamador@gmail.com>
2011-04-18 20:22:49 -0300

    initial project commit

---
+++ com.nmt.nmj.editor.plugin/src/com/nmt/nmj/editor/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.nmt.nmj.editor;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.nmt.nmj.editor.plugin.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ com.nmt.nmj.editor.plugin/src/com/nmt/nmj/editor/Perspective.java
@@ -0,0 +1,10 @@
+package com.nmt.nmj.editor;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
