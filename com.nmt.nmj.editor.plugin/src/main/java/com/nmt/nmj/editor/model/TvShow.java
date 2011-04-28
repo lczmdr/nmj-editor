3ad7b48ce8129ecec812dc094526317115fffe85
Diego López León <dieguitoll@gmail.com>
2011-04-28 19:20:16 -0300

    add new object model

---
+++ com.nmt.nmj.editor.plugin/src/main/java/com/nmt/nmj/editor/model/TvShow.java
@@ -0,0 +1,16 @@
+package com.nmt.nmj.editor.model;
+
+import java.util.List;
+
+public class TvShow {
+
+	private List<Episode> episodes;
+
+	public List<Episode> getEpisodes() {
+		return episodes;
+	}
+
+	public void setEpisodes(List<Episode> episodes) {
+		this.episodes = episodes;
+	}
+}
