5475d1d2b8e131f79af36211521f9bc7995c5cd0
Diego López León <dieguitoll@gmail.com>
2011-04-29 01:40:07 -0300

    add testing infraestructure

---
+++ com.nmt.nmj.editor.plugin.tests/src/test/java/com/nmt/nmj/editor/backend/RegexMovieDataScrapperTest.java
@@ -0,0 +1,15 @@
+package com.nmt.nmj.editor.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RegexMovieDataScrapperTest {
+
+    @Test
+    public void testGetTitle() {
+        fail("Not yet implemented");
+    }
+
+}
